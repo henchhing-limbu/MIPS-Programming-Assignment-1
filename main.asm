e08607346fa55ddd1f86fe5c08bb619c7fa02f48
henchhing-limbu <33561536+henchhing-limbu@users.noreply.github.com>
2017-11-12 23:58:12 -0500

    added a loop to check if the string is valid
    A loop is added where it goes through each character in the string and checks if the character is valid hexadecimal ASCII character or not. If it is not, then through a branch statement it completely informs system to end the whole program. When it goes through all of the characters, it now ends the loop.  #checking if the character is valid or not is still to be added

--- main.asm
+++ main.asm
@@ -1,26 +1,42 @@
-.data
+.data						#data declaration section
 	prompt: .asciiz "Enter a string: "
 	userInput: .space 9
-.text
+	errorMessage: .asciiz "\nInvalid hexadecimal number\n"
+	
+.text						#assembly language instructions
 	main:
 		#dsiplays the prompt saying the user to input data
-		li $v0, 4 		#tells the system to be ready for printing string
-		la $a0, prompt          #loads address of prompt to $a0
-		syscall			#prints the string
+		li $v0, 4 			#tells the system to be ready for printing string
+		la $a0, prompt          	#loads address of prompt to $a0
+		syscall				#prints the string
 		
 		#gets input from the user
 		li $v0, 8
 		la $a0, userInput
-		li $a1, 9		#specifies the number of bytes to read
+		li $a1, 9			#specifies the number of bytes to read
 		syscall
 
-		#displays the input from the suer (userInput)
+		#displays the input from the user (userInput)
 		li $v0, 4
 		la $a0, userInput
 		syscall
 		
-	#informs system to end main
+		#reading character in a string
+		la $t0, userInput
+		li $t1, 0          		 #$t1 is set to 0
+		loop:
+			lb $t2, 0($t0)   	 #loads the first character in userInput 
+			#Fix me 
+			add $t0, $t0, 1          #incremets the address to get next character
+			beq $t1, 8, end		 #if $t1 is equal to 8 then go to end 
+			add $t1, $t1, 1		 #increments the value of $t1
+			j loop 			 #loops again
+		exit:
+			li $v0, 4
+			la $a0, errorMessage
+			syscall 		 #prints the error message
+			li $v0, 10
+			syscall
+		end:				 #this is where we start converting hexadecimal to decimal	
 	li $v0, 10
-	syscall
-	
-	
+	syscall					 #informs system to end main
